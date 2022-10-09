19b2195201a586e14697f9006e7166b77b3a372e
Mohd Amir <Amirkhan98375@gmail.com>
2022-10-09 19:07:29 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import { TodoApp } from './components/Todo';
+import { Counter } from './Counter';
 
 function App() {
   return (
     <div className="App">
       <h2>Todo App with delete update on state</h2>
+      <Counter/>
     <TodoApp/>
     </div>
   );
---
+++ src/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+export const Counter = () => {
+    const [counter,setCounter]=useState(0)
+    const AddCount=()=>{
+        setCounter(counter+1)
+    }
+    const Decrease=()=>{
+        if(counter>0){
+            setCounter(counter-1)
+        }
+    }
+    const Reset=()=>{
+        setCounter(0)
+    }
+  return (
+    <div>
+        <p>counter:{counter}</p>
+<button onClick={AddCount}>Add</button>
+<button onClick={Decrease}>Sub</button>
+<button onClick={Reset}>Reset</button>
+    </div>
+  )
+}
